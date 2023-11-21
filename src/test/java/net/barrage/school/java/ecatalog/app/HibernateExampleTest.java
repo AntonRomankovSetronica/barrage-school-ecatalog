76f50b6eadd433a27e62c217130737a433abdc84
Anton Romankov <a.romankov@arammeem.com>
2023-11-21 10:07:53 +0300

    upd

--- src/test/java/net/barrage/school/java/ecatalog/app/HibernateExampleTest.java
+++ src/test/java/net/barrage/school/java/ecatalog/app/HibernateExampleTest.java
@@ -24,6 +24,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @ActiveProfiles("db")
 @SpringBootTest
 public class HibernateExampleTest {
@@ -57,15 +60,40 @@ public class HibernateExampleTest {
     // Transactional wraps this method with a session, we will need it to make "m.getProducts()" works.
     // U can comment it and check the error, should be: org.hibernate.LazyInitializationException: failed to lazily initialize a collection
     @Transactional(readOnly = true)
-    public void get_products_from_merchant() {
+    void get_products_from_merchant() {
         // Here we get just some first merchant
         var m = merchantRepository.findAll().iterator().next();
         // and try to access its products (products aren't fetched from db yet)
 
-        // getProducts returns Set<Product>, but it's nt a HashSet, it's PersistenceSet which is a proxy.
+        // getProducts returns Set<Product>, but it's not a HashSet, it's PersistenceSet which is a proxy.
+        Set<Product> products = m.getProducts();
         // Whenever u try to access it proxy will query the db asking for products for this merchant.
         // Without having session (@Transactional(readOnly = true)) the proxy can't understand what connection to use to make a db query.
-        System.out.println(new HashSet<>(m.getProducts()));
+        products.forEach(System.out::println);
+    }
+
+    /* If you want to understand why do we need equals and hashCode - consider following test! */
+    @Test
+    void understanding_EqualsAndHashCode() {
+        Set<Product> products = new HashSet<>();
+        Product beer = new Product()
+                .setId(new UUID(0, 1))
+                .setName("Beer");
+        Product cheeps = new Product()
+                .setId(new UUID(0, 1))
+                .setName("Cheeps");
+
+        // We put 2 different objects to our products set here...
+        products.add(beer);
+        products.add(cheeps);
+
+        // How do u think what size products will have now? Why?
+        assertEquals(2, products.size());
+
+        // Are these two objects equals or not? Why?
+        assertNotEquals(beer, cheeps);
+
+        // More or less good explanation of how hashmap & hashset works - https://www.javatpoint.com/working-of-hashmap-in-java
     }
 }
 
